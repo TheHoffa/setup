850be4a5dae9895195ba0651974cb6ae5731e409
TheHoffa <136639877+TheHoffa@users.noreply.github.com>
2024-06-06 15:39:22 +0200

    Update setup.sh
    bug fixes

--- setup.sh
+++ setup.sh
@@ -15,48 +15,49 @@ LINUX_DIR="$TOOLS_DIR/Linux"
 WINDOWS_DIR="$TOOLS_DIR/Windows"
 TUNNELING_DIR="$TOOLS_DIR/Tunneling"
 
-if [ -d "$TOOLS_DIR" ] || [ -d "$DESKTOP_DIR/Stand-Alone-1" ] || [ -d "$DESKTOP_DIR/Stand-Alone-2" ] || [ -d "$DESKTOP_DIR/Stand-Alone-3" ] || [ -d "$DESKTOP_DIR/AD-set" ]; then
-    echo "${ORANGE}[*]${NC}One or more of the specified folders already exist."
-    echo "${ORANGE}[*]${NC}Please remove or rename the existing folders before running this script again."
-    exit 1
-fi
-
 # Check if the file /etc/os-release exists
 if [ -f "/etc/os-release" ]; then
     # Source the file to get the variables
     . /etc/os-release
+    echo "${ORANGE}[*]${NC}Checking if you are running Kali Linux ${ORANGE}2024.2"
     # Check if the variable VERSION contains "2024.2"
     if [[ "$VERSION" == "2024.2" ]]; then
-        echo "${ORANGE}[*]${NC}Running Kali Linux ${GREEN}2024.2"
+        echo "${ORANGE}[*]${NC}You are running Kali Linux ${GREEN}2024.2"
     else
-        echo "${ORANGE}[*]${RED}ERROR${NC}: Wrong version of Kali!"
+        echo "${ORANGE}[*]${RED}ERROR${NC}: Wrong Kali version!"
         echo -n "${ORANGE}[*]${NC}Do you want to continue anyway? (${GREEN}yes${NC}/${RED}no${NC}): "
         read choice
         case "$choice" in
-            yes|YES|y|Y ) echo "Continuing with the script...";;
-            * ) echo "Exiting script."; exit 1;;
+            yes|YES|y|Y ) echo "Continuing with the setup...";;
+            * ) echo "Exiting setup."; exit 1;;
         esac
     fi
 else
-    echo "${RED}File /etc/os-release not found. This script is intended for Kali Linux."
+    echo "${RED}File /etc/os-release not found. This setup is intended for Kali Linux."
     exit 1
 fi
 
+# Check if setup has already been ran before
+if [ -d "$TOOLS_DIR" ] || [ -d "$DESKTOP_DIR/Stand-Alone-1" ] || [ -d "$DESKTOP_DIR/Stand-Alone-2" ] || [ -d "$DESKTOP_DIR/Stand-Alone-3" ] || [ -d "$DESKTOP_DIR/AD-set" ]; then
+    echo ""
+    echo "${ORANGE}[*]${NC}One or more of the specified folders already exist."
+    echo "${ORANGE}[*]${NC}Please remove or rename the existing folders before running this setup again."
+    exit 1
+fi
 
-
-echo "${ORANGE}[*]${NC}This script will create some folders on your ${ORANGE}desktop${NC}, install some ${ORANGE}tools${NC} on your ${ORANGE}system ${NC}and add some ${ORANGE}aliases${NC} to your ${ORANGE}.zshrc ${NC}file."
+echo "${ORANGE}[*]${NC}This setup will create some folders on your ${ORANGE}desktop${NC}, install some ${ORANGE}tools${NC} on your ${ORANGE}system ${NC}and add some ${ORANGE}aliases${NC} to your ${ORANGE}.zshrc ${NC}file."
 
 # Prompt the user for continuation
-
 echo -n "${ORANGE}[*]${NC}Do you wish to continue? (${GREEN}yes${NC}/${RED}no${NC}): "
 read choice
 case "$choice" in 
-  yes|y ) echo "${ORANGE}[*]${GREEN}Continuing${NC} with the script...";;
-  no|n ) echo "${ORANGE}[*]${RED}Exiting script."; exit 1;;
-  * ) echo "${ORANGE}[*]${RED}Invalid response. Exiting script.${NC}"; exit 1;;
+  yes|y ) echo "${ORANGE}[*]${GREEN}Continuing${NC} with the setup...";;
+  no|n ) echo "${ORANGE}[*]${RED}Exiting setup."; exit 1;;
+  * ) echo "${ORANGE}[*]${RED}Invalid response. Exiting setup.${NC}"; exit 1;;
 esac
 
 # Ask for password upfront to avoid repetitive sudo password prompts
+echo ""
 echo "${ORANGE}[*]${NC}Please enter your ${RED}password${NC} to authenticate for sudo rights during instalation:"
 sudo -v
 # Check if sudo access is successful
@@ -77,12 +78,17 @@ echo "alias pyserv='ls; python -m http.server 80'" >> "$ZSHRC_FILE"
 echo "alias smbserv='sudo impacket-smbserver'" >> "$ZSHRC_FILE"
 echo "alias load='sudo xdg-open'" >> "$ZSHRC_FILE"
 echo "alias multi='sudo msfconsole -qx '\''use multi/handler; set PAYLOAD generic/shell_reverse_tcp; set LHOST 1.1.1.1; set LPORT 5555 '\'''" >> "$ZSHRC_FILE"
+echo "alias revshells='firefox https://www.revshells.com/ &'" >> "$ZSHRC_FILE"
+echo "alias psrevshell='python /home/kali/Desktop/Tools/Windows/Powershell-Base64-Reverse-Shell-Generator/PowerShellReverseShellGen.py'" >> $ZSHRC_FILE"
+
 echo ""
 echo "${ORANGE}[*]${NC}The following ${GREEN}aliases${NC} are added to ${GREEN}$ZSHRC_FILE:"
 echo "${ORANGE}[*]${GREEN}pyserv ${NC}(starts a python server on port 80)"
 echo "${ORANGE}[*]${GREEN}smbserv ${NC}(starts impacket-smbserver)"
 echo "${ORANGE}[*]${GREEN}load ${NC}(xdg-open)"
 echo "${ORANGE}[*]${GREEN}multi ${NC}(starts msf multi/handler)"
+echo "${ORANGE}[*]${GREEN}revshells ${NC}(starts firefox and navigates to revshells.com)"
+echo "${ORANGE}[*]${GREEN}psrevshell ${NC}(Generates a base64 encoded PowerShell reverse shell"
 
 echo ""
 # Update the system
@@ -91,7 +97,8 @@ sudo apt update -qq > /dev/null 2>&1
 echo "${ORANGE}[*]${NC}Running ${GREEN}apt upgrade"
 sudo apt upgrade -y -qq > /dev/null 2>&1
 echo "${ORANGE}[*]${NC}Installing dependencies"
-sudo apt install golang-go autorecon seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y -qq > /dev/null 2>&1
+echo "${ORANGE}[*]${NC}This may take some time..."
+sudo apt install golang-go enum4linux-ng autorecon seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y -qq > /dev/null 2>&1
 echo ""
 # Check if the Desktop directory exists
 if [ ! -d "$DESKTOP_DIR" ]; then
@@ -195,7 +202,7 @@ echo ""
 # Countdown function
 countdown() {
     for i in {10..1}; do
-        echo -n "${ORANGE}[*]${NC} Exam setup ${GREEN}completed${NC} Script will end in ${ORANGE}$i${NC} seconds..."
+        echo -n "${ORANGE}[*]${NC} Exam setup ${GREEN}completed${NC} setup will end in ${ORANGE}$i${NC} seconds..."
         sleep 1
         echo -ne "\r\033[K"  # Clear the line
     done
@@ -212,5 +219,4 @@ read choice
 # Execute commands after countdown
 echo "${ORANGE}[*]${NC} Executing ${ORANGE}'exec zsh'${NC} command..."
 exec zsh
-echo "${ORANGE}[*]${NC} Clearing the screen..."
-clear
+
